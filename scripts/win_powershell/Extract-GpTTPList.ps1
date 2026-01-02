dd715c0ec21802c846ccf821a3fc6cfad64d742f
Logan Bennett <logan@localhost>
2026-01-02 16:39:35 -0500

    feat(script/playbooks): Added two new scripts and multiple playbooks for known malware using public sources; playbooks are in draft.

---
+++ scripts/win_powershell/Extract-GpTTPList.ps1
@@ -0,0 +1,39 @@
+﻿
+# Written by Logan Bennett
+# This is a simple script to extract TTPs from a report, list, file, etc.
+# For files, the script only gets the raw content; pdfs, docx, etc. aren't supported. Text files work fine
+# You can think of this as a strings parser
+
+[CmdletBinding()]
+Param(
+    [Parameter(ParameterSetName="Raw")]
+    [String]$String,
+
+    [Parameter(ParameterSetName="Content")]
+    [String]$File
+)
+
+Function Extract-TTPs
+{
+    Param(
+        [Parameter(Mandatory=$True)]
+        [String]$Content
+    )
+
+    ($Content | Select-String -Pattern 'T[A0-9]{4,5}[.0-9]{0,4}' -AllMatches).Matches.Value | Sort-Object -Unique
+}
+
+if ($String)
+{
+    Extract-TTPs -Content $String
+}
+elseif ($File -and (Test-Path $File))
+{
+    [string]$FileContent = Get-Content $File -ErrorAction Stop
+
+    Extract-TTPs -Content $FileContent
+}
+else
+{
+    Write-Error "There was a problem accessing the file; please make sure your path is correct."
+}
---
+++ scripts/win_powershell/Set-GpSMBHardeningStandard.ps1
@@ -0,0 +1,97 @@
+﻿#Requires -RunAsAdministrator
+
+# Written by Logan Bennett
+# This script sets modern hardening standards for SMB, minus the firewall rules
+# TODO: Some of these commands are only avaialble on Windows 11 and Windows Server 2025, so need to add OS version handeling
+# For now this script is good for newer deployments
+
+[CmdletBinding()]
+Param(
+    [switch]$ServerConfig,
+    [siwtch]$ClientConfig
+)
+
+Function Remove-SMBVersion1
+{
+    # Begins removing insecure protocols
+    $SMBFeatures = @('FS-SMB1','FS-SMB1-CLIENT','FS-SMB1-SERVER')
+    Get-WindowsFeature -Name $SMBFeatures | Uninstall-WindowsFeature -Remove -IncludeManagementTools
+}
+
+#### Server Configuration ####
+
+if ($ServerConfig)
+    {
+
+    # Checks if the service is running, if not exits
+    $SMBService = Get-Service -Name 'LanmanServer' -ErrorAction Stop
+
+    if ($SMBService.Status -eq 'Running')
+    {
+        # Removes SMB1 from the system
+        Remove-SMBVersion1
+
+        # Now disables SMBv1 & SMB/QUIC and turns on SMBv2/3
+        Set-SmbServerConfiguration -EnableSMB1Protocol $False -EnableSMB2Protocol $True -EnableSMBQUIC $False
+
+        # Enforces encryption over all shares and enables signing
+        Set-SmbServerConfiguration -EncryptData $True -EnableSecuritySignature $True -RequireSecuritySignature $True
+
+        # Sets a decent standard of encryption for the encryption algorithms
+        Set-SmbServerConfiguration -EncryptionCiphers 'AES_128_GCM, AES_256_GCM'
+
+        # Now restricts unencrypted access
+        Set-SmbServerConfiguration -RejectUnencryptedAccess $True
+
+        # Sets the only accepted SMB versions as 3.0
+        Set-SmbServerConfiguration -Smb2DialectMin SMB300 -Smb2DialectMax SMB311
+
+        # Sets an auth limiter to 10 seconds (increased from default 2 seconds)
+        Set-SmbServerConfiguration -EnableAuthRateLimiter $True -InvalidAuthenticationDelayTimeInMs 10000 # 10 seconds in miliseconds
+
+        # Sets auditing levels for suspicious behavior
+        Set-SmbServerConfiguration -AuditInsecureGuestLogon $True -AuditClientDoesNotSupportEncryption $True -AuditSmb1Access $True -AuditClientDoesNotSupportSigning $True
+
+        # Finally, sets forced logoff when user logon hours expire
+        Set-SmbServerConfiguration -EnableForcedLogoff $True
+    }
+    else
+    {
+        Write-Error "The SMB service (Server / LanmanServer) is not running."
+    }
+}
+
+
+##### Client Configuration #####
+
+if($ClientConfig)
+{
+    # Removes SMB1 from the system
+    Remove-SMBVersion1
+
+    # Blocks NTLM for outbound authentication
+    Set-SmbClientConfiguration -BlockNTLM $True 
+
+    # Enforces encryption usage
+    Set-SmbClientConfiguration -RequireEncryption $True
+
+    # Enforces ciphers
+    Set-SmbClientConfiguration -EncryptionCiphers 'AES_128_GCM, AES_256_GCM'
+
+    # Enforces signing
+    Set-SmbClientConfiguration -RequireSecuritySignature $True
+
+    # Turns off mailslots
+    Set-SmbClientConfiguration -EnableMailslots $False
+
+    # Disables SMB via QUIC
+    Set-SmbClientConfiguration -EnableSMBQUIC $False
+
+    # Disables guest logins
+    Set-SmbClientConfiguration -EnableInsecureGuestLogons $False
+}
+
+if (-not ($ServerConfig -and $ClientConfig))
+{
+    Write-Error "You must specify one or both of the available switches: ServerConfig / ClientConfig"
+}
